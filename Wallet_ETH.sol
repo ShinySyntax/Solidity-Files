d5ef088313e3ecd25f5bef185ab9bfc9b99b4ef0
kindlyman343423 <kindlyman343423@gmail.com>
2021-01-30 13:31:35 -0500

    Create Wallet_ETH.sol

---
+++ Wallet_ETH.sol
@@ -0,0 +1,17 @@
+pragma solidity 0.5.1;
+contract Wallet_ETH {
+    mapping(address => uint256) public balances;
+    address payable wallet;
+    event Purchase(address indexed _buyer, uint256 _amount);
+    constructor(address payable _wallet) public {
+        wallet = _wallet;
+    }
+    function buyToken() public payable{
+        //buy a token;
+        balances[msg.sender] += 1;
+        //send ether to the wallet
+        wallet.transfer(msg.value);
+        emit Purchase(msg.sender, 1);
+
+    }
+}
