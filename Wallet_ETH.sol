d037e225c0336cdddc845816ea802498c6559254
kindlyman343423 <kindlyman343423@gmail.com>
2021-01-30 14:31:55 -0500

    Apply suggestions from code review
    Co-authored-by: Aram Services <aramservices198@hotmail.com>

--- Wallet_ETH.sol
+++ Wallet_ETH.sol
@@ -2,11 +2,11 @@ pragma solidity 0.5.1;
 contract Ethereum_Wallet {
     mapping(address => uint256) public balances;
     address payable wallet;
-    event Purchase(address indexed _buyer, uint256 _amount);
+    event bought(address indexed _buyer, uint256 _amount);
     constructor(address payable _wallet) public {
         wallet = _wallet;
     }
-    function buyToken() public payable{
+    function buyEthereum() public payable{
         //buy a token;
         balances[msg.sender] += 1;
         //send ether to the wallet
